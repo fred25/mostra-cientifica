43bba255c75d6f9cb504a4365c390676a7382cff
Frederico Mattos <fredericomattos25@gmail.com>
2023-05-22 19:09:25 -0300

    i will die

--- test_page/test_code.js
+++ test_page/test_code.js
@@ -1,7 +1,13 @@
 var image = document.getElementById("image");
 
+var but_1 = document.getElementById("but_1");
+
 const dtype = new URLSearchParams(window.location.search).get("dtype");
 //data types: "picture", "audio", "number"
-dir = "../../UI/Python_codes/" + dtype + "/Data/0/00000_00000.ppm";
+dir = "../../UI/js_data/Image/0/00000_00000.jpg";
 
 image.src = dir;
+
+but_1.addEventListener("click", () => {
+    image.src = "../../UI/js_data/Image/0/00000_00017.jpg";
+});
